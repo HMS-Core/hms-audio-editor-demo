b390572d048935452f7b75e73257c6faa3869cb2
xuxiapu <114389983+xuxiapu@users.noreply.github.com>
2023-01-06 14:04:16 +0800

    Update AiDubbingAudioActivity.java

--- apk/app/src/main/java/com/huawei/hms/audioeditor/demo/AiDubbingAudioActivity.java
+++ apk/app/src/main/java/com/huawei/hms/audioeditor/demo/AiDubbingAudioActivity.java
@@ -306,7 +306,6 @@ public class AiDubbingAudioActivity extends AppCompatActivity
     private void stopAiDubbing() {
         if (mEngine != null) {
             mEngine.stop();
-            mEngine = null;
         }
     }
 
